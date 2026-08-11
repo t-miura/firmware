949058a76c8af9c71e268fbe4f496ca2567d49e6
Tadayoshi MIURA <11958457+t-miura@users.noreply.github.com>
2026-08-11 21:02:13 +0900

    move wio-e5 gps/uart specific code to proper location

--- variants/stm32/wio-e5/variant.cpp
+++ variants/stm32/wio-e5/variant.cpp
@@ -1,8 +1,5 @@
-#include <Arduino.h>
-
-#if defined(ARDUINO_LORA_E5_MINI) || defined(ARDUINO_LORA_E5_DEV_BOARD)
-
 #include "PinNames.h"
+#include <Arduino.h>
 
 #ifdef HAL_UART_MODULE_ENABLED
 const PinMap PinMap_UART_TX[] = {{PA_2, USART2, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF7_USART2)},
@@ -14,6 +11,4 @@ const PinMap PinMap_UART_RX[] = {{PA_3, USART2, STM_PIN_DATA(STM_MODE_AF_PP, GPI
                                  {PA_10, USART1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF7_USART1)},
                                  {PB_7, USART1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF7_USART1)},
                                  {NC, NP, 0}};
-#endif
-
 #endif
